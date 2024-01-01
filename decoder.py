ad0dc5c33566f328e1912a9742fe00b463e82911
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2024-01-02 00:44:53 +0330

    adding Attention block

--- decoder.py
+++ decoder.py
@@ -4,25 +4,29 @@ from torch.nn import functional as F
 from attention import SelfAttention
 
 class VAE_AttentionBlock(nn.Module):
-    
+    def __init__(self, channs: int):
+        super().__init__()
 
+        self.groupnorm = nn.GroupNorm(32, channs)
+        
 
 
 
 class VAE_ResBlock(nn.Module):
-    def __init__(self, in_chan, out_chan):
+    
+    def __init__(self, in_chann: int, out_chann: int):
         super().__init__()
 
-        self.groupnorm_1 = nn.GroupNorm(32, in_chan)
-        self.conv_1 = nn.Conv2d(in_chan, out_chan, kernel_size=3, padding=1)
+        self.groupnorm_1 = nn.GroupNorm(32, in_chann)
+        self.conv_1 = nn.Conv2d(in_chann, out_chann, kernel_size=3, padding=1)
         
-        self.groupnorm_2 = nn.GroupNorm(32, out_chan)
-        self.conv_2 = nn.Conv2d(out_chan, out_chan, kernel_size=3, padding=1)
+        self.groupnorm_2 = nn.GroupNorm(32, out_chann)
+        self.conv_2 = nn.Conv2d(out_chann, out_chann, kernel_size=3, padding=1)
 
-        if in_chan == out_chan:
+        if in_chann == out_chann:
             self.res_layer = nn.Identity()
         else:
-            self.res_layer = nn.Conv2d(in_chan, out_chan, kernel_size=1, padding=0)
+            self.res_layer = nn.Conv2d(in_chann, out_chann, kernel_size=1, padding=0)
         
         def forward(self, x: torch.Tensor) -> torch.Tensor:
             residue = x
